12e319f6092c11897d9e2f3458cc5874a0a6805f
posicionamientomurcia <67927542+posicionamientomurcia@users.noreply.github.com>
2023-03-07 11:08:59 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,23 +24,23 @@ export default function IndexPage() {
       <section className="container grid max-w-4xl items-center gap-6 pt-12 pb-8 sm:gap-8 md:py-36">
         <div className="flex flex-col gap-6 sm:gap-8 ">
           <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Transcribe any video for free.
+            تفريغ نصي للفيديوهات
           </h1>
           <p className="mx-auto max-w-sm text-center text-lg text-slate-700 dark:text-slate-400 sm:text-xl md:max-w-xl">
-            Turn videos into text in minutes. Whether it's your own files or a
-            YouTube video, we've got you covered.
+            تحويل مقاطع الفيديو والملفات الصوتية لنصوص
+            وكذلك مقاطع يوتيوب،
           </p>
         </div>
 
         <div className="flex justify-center gap-4">
           <Link href={siteConfig.links.video} className={buttonVariants()}>
-            Upload File
+            ملف صوتي
           </Link>
           <Link
             href={siteConfig.links.youtube}
             className={buttonVariants({ variant: "outline" })}
           >
-            Youtube Link
+            عنوان اليوتيوب
           </Link>
         </div>
       </section>
